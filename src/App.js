40ae337d486ba37439135ed8a6498e5dd3d37166
dgekaa <dgeka_vip@mail.ru>
2021-05-03 11:01:08 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,87 +1,240 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import QUERY from "./query";
 
-const Line = styled.div`
-    cursor: pointer;
+const anim = keyframes`
+   0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+`;
+
+const Wrapper = styled.div`
     display: flex;
-    border-bottom: 1px solid #eee;
-    padding: 10px;
-    &:hover {
-      background: rgba(111, 111, 111, 0.05);
-      transition: 0.3s ease background;
-    }
-    @media (max-width: 760px) {
-    }
+    margin: 0 auto;
+    width: 1000px;
+    font-family: sans-serif;
+    border-radius: 5px;
   `,
-  Area = styled.span`
-    display: flex;
-    padding: 0 10px;
-    width: 100px;
-    justify-content: center;
-    @media (max-width: 760px) {
-    }
+  LeftMenu = styled.div`
+    width: 150px;
+    background-color: #eee;
+    padding-right: 10px;
   `,
-  AppointmentsWrap = styled.div`
-    padding: 5px;
+  Content = styled.div`
     display: flex;
-    width: 100%;
+    flex: 1;
+    border-radius: 5px;
+    background-color: #fff;
     flex-direction: column;
-    background: rgba(32, 234, 77, 0.4);
-    @media (max-width: 760px) {
+  `,
+  Button = styled.div`
+    display: flex;
+    background-color: #fff;
+    flex: 1;
+    margin-bottom: 10px;
+    padding: 10px 0;
+    justify-content: center;
+    cursor: pointer;
+    font-size: 20px;
+    border-radius: 5px;
+    background-color: ${(props) => (props.active ? "green" : "#fff")};
+    color: ${(props) => (props.active ? "#fff" : "#000")};
+    &:hover {
+      background-color: green;
+      color: #fff;
+      transition: 0.2s ease all;
     }
   `,
-  Appointments = styled.div`
-    background: #fff;
-    padding: 5px;
-    margin: 5px;
+  Loader = styled.div`
+    position: fixed;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 26px;
+    color: green;
+    height: 100%;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+  `,
+  Load = styled.div`
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  `,
+  LoadBlockFirst = styled.div`
+    position: absolute;
+    border: 4px solid green;
+    opacity: 1;
+    border-radius: 50%;
+    animation: ${anim} 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  `,
+  LoadBlockSecond = styled.div`
+    position: absolute;
+    border: 4px solid green;
+    opacity: 1;
+    border-radius: 50%;
+    animation: ${anim} 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+    animation-delay: -0.5s;
+  `,
+  Table = styled.div`
     display: flex;
     flex-direction: row;
-    padding: 0;
-    @media (max-width: 760px) {
+    padding: 5px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    &:nth-child(odd) {
+      background-color: rgba(0, 0, 0, 0.01);
     }
   `,
-  Appointment = styled.span`
+  TableHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 5px;
+    border-bottom: 2px solid #000;
+  `,
+  Row = styled.div`
+    flex: 1;
+    padding: 5px;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    width: ${({ width }) => (width ? width + "px" : "100px")};
-    color: ${({ color }) => (color ? color : "")};
-    @media (max-width: 760px) {
-    }
   `,
-  Offers = styled.div`
-    width: 100px;
-    @media (max-width: 760px) {
-    }
-  `,
-  OfferWrap = styled.div`
-    margin: 5px 0;
-    @media (max-width: 760px) {
-    }
-  `,
-  Offer = styled.p`
-    width: 120px;
-    margin: 0;
-    padding: 0;
+  RowHeader = styled.div`
+    flex: 1;
+    font-weight: bold;
+    padding: 5px;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    @media (max-width: 760px) {
+  `,
+  ShowAppointments = styled.div`
+    position: relative;
+    bottom: 3px;
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    border-radius: 5px;
+    color: #fff;
+    padding: 3px;
+    width: 50px;
+    background-color: green;
+    &:hover {
+      background-color: #24b424;
+      transition: 0.1s ease background-color;
     }
+  `,
+  Modal = styled.div`
+    flex-direction: column;
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    background-color: #fff;
+    padding: 20px;
+    max-height: 100%;
+    min-height: 100%;
+    overflow-y: auto;
+  `,
+  AppointmentsWrapHeader = styled.div`
+    margin: 0 20px;
+    display: flex;
+    flex-direction: row;
+    font-weight: bold;
+    margin-top: 50px;
+    border-bottom: 2px solid rgba(0, 0, 0, 0.625);
+    padding: 10px;
+  `,
+  AppointmentsWrap = styled.div`
+    &:nth-child(odd) {
+      background-color: rgba(0, 0, 0, 0.03);
+    }
+    margin: 0 20px;
+    display: flex;
+    flex-direction: row;
+    padding: 10px;
+  `,
+  AppointmentsRow = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  `,
+  AppointmentsRowServices = styled.div`
+    width: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  `,
+  OfferFirst = styled.span`
+    width: 200px;
+    white-space: nowrap;
+    overflow: hidden;
+    padding: 5px;
+    text-overflow: ellipsis;
+    flex-direction: row;
+  `,
+  Offer = styled.span`
+    width: 90px;
+    white-space: nowrap;
+    overflow: hidden;
+    padding: 5px;
+    text-overflow: ellipsis;
+    flex-direction: row;
+  `,
+  CloseBtn = styled.div`
+    line-height: 26px;
+    border-radius: 5px;
+    padding: 5px 5px 7px 5px;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.625);
+    position: fixed;
+    cursor: pointer;
+    display: flex;
+    color: #000;
+    top: 20px;
+    right: 20px;
+    font-size: 26px;
+    &:hover {
+      border: 1px solid red;
+      color: red;
+      transition: 0.1s ease all;
+    }
+  `,
+  UserData = styled.span`
+    font-weight: bold;
+    margin-right: 15px;
   `;
 
 const App = () => {
   const type = { Master: "Master", Client: "Client" };
-
-  const [masters, setMasters] = useState(""),
-    [clients, setClients] = useState(""),
-    [clientsLoading, setClientsLoading] = useState(true),
-    [mastersLoading, setMastersLoading] = useState(true);
+  const [clickedBtn, setClickedBtn] = useState(""),
+    [DATA, setDATA] = useState(null),
+    [isLoading, setIsLoading] = useState(false),
+    [showAppointment, setShowAppointment] = useState(""),
+    [selectedUser, setSelectedUser] = useState("");
 
   const loadUsers = (usersType) => {
-    usersType === "Master" ? setMastersLoading(true) : setClientsLoading(true);
+    setIsLoading(true);
     QUERY({
       query: `query {users (type: ${usersType}, first:${100}) { 
         data { 
@@ -116,166 +269,181 @@ const App = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        usersType === "Master"
-          ? setMastersLoading(false)
-          : setClientsLoading(false);
-
-        usersType === "Master"
-          ? setMasters(data.data.users.data)
-          : setClients(data.data.users.data);
+        setDATA(data.data.users.data);
+        setIsLoading(false);
       })
       .catch((err) => {
-        usersType === "Master"
-          ? setMastersLoading(false)
-          : setClientsLoading(false);
-        console.log(err);
+        alert("error");
+        setIsLoading(false);
       });
   };
 
   useEffect(() => {
+    setClickedBtn("clients");
     loadUsers(type.Client);
-    loadUsers(type.Master);
   }, []);
 
   useEffect(() => {
-    console.log(masters, "-----masters");
-    console.log(clients, "-----clients");
-  }, [masters, clients]);
+    showAppointment
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [showAppointment]);
+
+  useEffect(() => {
+    console.log(DATA, "-data");
+  }, [DATA]);
 
   return (
     <div className="App">
-      <h1>PROFMA</h1>
-      <h2>Мастера</h2>
-      {mastersLoading && <div>Идет загрузка данных...</div>}
-      {masters &&
-        masters.length &&
-        masters.map((el, i) => (
-          <div>
-            <Line
-              onClick={(e) => {
-                if (el.master_appointments.length) {
-                  e.currentTarget.nextElementSibling.style.display === "flex"
-                    ? (e.currentTarget.nextElementSibling.style.display =
-                        "none")
-                    : (e.currentTarget.nextElementSibling.style.display =
-                        "flex");
-                }
-              }}
-            >
-              {el.master_appointments.length ? "!" : ""}
-              <Area>{el.id || "-"}</Area>
-              <Area>{el.profile.name || "-"}</Area>
-              <Area>{el.profile.about_me || "-"}</Area>
-              <Area>{el.profile.email || "-"}</Area>
-              <Area>{el.profile.city ? el.profile.city.name : "-"}</Area>
-            </Line>
-            <div style={{ display: "none" }}>
-              {el.master_appointments.length && (
-                <AppointmentsWrap>
-                  {el.master_appointments.map((appointment, i) => (
-                    <Appointments>
-                      <Appointment width={30} color={"red"}>
-                        {appointment.id}
-                      </Appointment>
-                      <Appointment>
-                        {appointment.client.profile.name}
-                      </Appointment>
-                      <Appointment>
-                        {appointment.client.profile.email || "-"}
-                      </Appointment>
-                      <Appointment>{appointment.status}</Appointment>
-                      <Appointment>{appointment.date}</Appointment>
-                      <Appointment>{appointment.time}</Appointment>
-                      <Offers>
-                        {appointment.offers.length &&
-                          appointment.offers.map((offer, i) => (
-                            <OfferWrap>
-                              <Offer>{offer.service.name}</Offer>
-                              <Offer>
-                                {offer.price_by_pack.price} руб.
-                                {offer.price_by_pack.duration} мин.
-                              </Offer>
-                            </OfferWrap>
-                          ))}
-                      </Offers>
-                    </Appointments>
-                  ))}
-                </AppointmentsWrap>
-              )}
-            </div>
-          </div>
-        ))}
+      <Wrapper>
+        <LeftMenu>
+          <Button
+            onClick={() => {
+              setClickedBtn("clients");
+              loadUsers(type.Client);
+            }}
+            active={clickedBtn === "clients"}
+          >
+            Клиенты {DATA[0].type === "Client" && `(${DATA.length})`}
+          </Button>
+          <Button
+            onClick={() => {
+              setClickedBtn("masters");
+              loadUsers(type.Master);
+            }}
+            active={clickedBtn === "masters"}
+          >
+            Мастера {DATA[0].type === "Master" && `(${DATA.length})`}
+          </Button>
+        </LeftMenu>
+        <Content>
+          {DATA && DATA.length && (
+            <TableHeader>
+              <RowHeader>Id</RowHeader>
+              <RowHeader>Имя</RowHeader>
+              <RowHeader>Обо мне</RowHeader>
+              <RowHeader>Email</RowHeader>
+              <RowHeader>Город</RowHeader>
+              <RowHeader>Записи</RowHeader>
+            </TableHeader>
+          )}
+          {DATA &&
+            DATA.length &&
+            DATA.map((el, index) => (
+              <Table>
+                <Row>{el.id}</Row>
+                <Row>
+                  {el.profile ? (el.profile.name ? el.profile.name : "-") : "-"}
+                </Row>
+                <Row>
+                  {el.profile
+                    ? el.profile.about_me
+                      ? el.profile.about_me
+                      : "-"
+                    : "-"}
+                </Row>
+                <Row>
+                  {el.profile
+                    ? el.profile.email
+                      ? el.profile.email
+                      : "-"
+                    : "-"}
+                </Row>
+                <Row>
+                  {el.profile
+                    ? el.profile.city
+                      ? el.profile.city.name
+                      : "-"
+                    : "-"}
+                </Row>
+                <Row>
+                  {el.client_appointments.length !== 0 ||
+                  el.master_appointments.length !== 0 ? (
+                    <ShowAppointments
+                      onClick={() => {
+                        window.scrollTo(0, 0);
+                        setSelectedUser(el);
+                        setShowAppointment(
+                          el.client_appointments.length !== 0
+                            ? el.client_appointments
+                            : el.master_appointments
+                        );
+                      }}
+                    >
+                      {el.client_appointments.length ||
+                        el.master_appointments.length}
+                    </ShowAppointments>
+                  ) : (
+                    "-"
+                  )}
+                </Row>
+              </Table>
+            ))}
+        </Content>
 
-      <h2>Клиенты</h2>
-      {clientsLoading && <div>Идет загрузка данных...</div>}
-      {clients &&
-        clients.length &&
-        clients.map((el, i) => (
-          <div>
-            <Line
-              onClick={(e) => {
-                if (el.client_appointments.length) {
-                  e.currentTarget.nextElementSibling.style.display === "flex"
-                    ? (e.currentTarget.nextElementSibling.style.display =
-                        "none")
-                    : (e.currentTarget.nextElementSibling.style.display =
-                        "flex");
-                }
-              }}
-            >
-              {el.client_appointments.length ? "!" : ""}
-              <Area>{el.id || "-"}</Area>
-              <Area>{el.profile ? el.profile.name : "-"}</Area>
-              <Area>{el.profile ? el.profile.about_me : "-"}</Area>
-              <Area>{el.profile ? el.profile.email : "-"}</Area>
-              <Area>
-                {el.profile
-                  ? el.profile.city
-                    ? el.profile.city.name
-                    : "-"
-                  : "-"}
-              </Area>
-            </Line>
-            <div style={{ display: "none" }}>
-              {el.client_appointments.length && (
-                <AppointmentsWrap>
-                  {el.client_appointments.map((appointment, i) => (
-                    <Appointments>
-                      <Appointment width={30} color={"red"}>
-                        {appointment.id}
-                      </Appointment>
-                      <Appointment>
-                        {appointment.master.profile
-                          ? appointment.master.profile.name
-                          : "-"}
-                      </Appointment>
-                      <Appointment>
-                        {appointment.master.profile
-                          ? appointment.master.profile.email
-                          : "-"}
-                      </Appointment>
-                      <Appointment>{appointment.status}</Appointment>
-                      <Appointment>{appointment.date}</Appointment>
-                      <Appointment>{appointment.time}</Appointment>
-                      <Offers>
-                        {appointment.offers.length &&
-                          appointment.offers.map((offer, i) => (
-                            <OfferWrap>
-                              <Offer>{offer.service.name}</Offer>
-                              <Offer>
-                                {offer.price_by_pack.price} руб.
-                                {offer.price_by_pack.duration} мин.
-                              </Offer>
-                            </OfferWrap>
-                          ))}
-                      </Offers>
-                    </Appointments>
-                  ))}
-                </AppointmentsWrap>
-              )}
+        {showAppointment && (
+          <Modal>
+            <CloseBtn onClick={() => setShowAppointment(null)}>
+              закрыть
+            </CloseBtn>
+            <div>
+              <UserData> UserId: {selectedUser.id}</UserData>
+              <UserData>
+                Name: {selectedUser.profile ? selectedUser.profile.name : "-"}
+              </UserData>
+              <UserData>
+                Email: {selectedUser.profile ? selectedUser.profile.email : "-"}
+              </UserData>
             </div>
-          </div>
-        ))}
+            <AppointmentsWrapHeader>
+              <AppointmentsRow>Id </AppointmentsRow>
+              <AppointmentsRow> Дата </AppointmentsRow>
+              <AppointmentsRow> Время </AppointmentsRow>
+              <AppointmentsRow> Статус </AppointmentsRow>
+              <AppointmentsRowServices> Услуги </AppointmentsRowServices>
+            </AppointmentsWrapHeader>
+            {showAppointment.map((el, index) => {
+              console.log(el, "---el");
+              return (
+                <AppointmentsWrap>
+                  <AppointmentsRow> {el.id} </AppointmentsRow>
+                  <AppointmentsRow> {el.date} </AppointmentsRow>
+                  <AppointmentsRow> {el.time} </AppointmentsRow>
+                  <AppointmentsRow> {el.status} </AppointmentsRow>
+                  <AppointmentsRowServices>
+                    <div style={{ flexDirection: "row" }}>
+                      {el.offers.length
+                        ? el.offers.map((el, i) => (
+                            <div
+                              style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                margin: "5px",
+                              }}
+                            >
+                              <OfferFirst>{el.service.name}</OfferFirst>
+                              <Offer>{el.price_by_pack.duration} мин.</Offer>
+                              <Offer>{el.price_by_pack.price} руб.</Offer>
+                            </div>
+                          ))
+                        : "-"}
+                    </div>
+                  </AppointmentsRowServices>
+                </AppointmentsWrap>
+              );
+            })}
+          </Modal>
+        )}
+
+        {isLoading && (
+          <Loader>
+            <Load>
+              <LoadBlockFirst />
+              <LoadBlockSecond />
+            </Load>
+          </Loader>
+        )}
+      </Wrapper>
     </div>
   );
 };
